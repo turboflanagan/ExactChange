9ed588f92e7793de3ec4a16e25d06ed36519fe86
turboflanagan <turboflanagan@gmail.com>
2016-02-16 15:48:54 -0500

    remove parseFloat()
    Replace parseFloat() method by multiplying values by 100 to remove the
    decimal in < dollar values.

--- exact-change.js
+++ exact-change.js
@@ -1,35 +1,53 @@
 var currencyValue = [0.01,0.05,0.10,0.25,1,5,10,20,100];
-var drawerStart = [
-    ["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], 
-    ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], 
-    ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]
-    ];
+// var drawerStart = [
+//     ["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], 
+//     ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], 
+//     ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]
+//     ];
 
 function drawer(price, cash, cid) {
-  var change = [];
-  var drawer = cid;
-  totalDrawerCash = 0;
-  console.log(drawerStart);
-  // Here is your change, ma'am.
-  change = parseFloat(cash - price);
-  for (i=0; i<drawerStart.length; i++){
-    totalDrawerCash += parseFloat(drawerStart[i][1].toFixed(2)); 
-    // adds the amount of each demon and incriments totalDrawerCash 
-    // on each iteration through the loop.  .toFixed sets the number
-    // to 2 decimals, but because it turns the number into a string, 
-    // we run parseFloat() on it to save it as a number so we can do math on it.
-    if(totalDrawerCash < change){
-      return("Insufficient Funds");
-    }else if(totalDrawerCash == change){
-      return("Closed");
-    }else{
-      return change;
+  //parseFloat() is not returning decimals properly. Converting to factor of 100.
+  // Convert back by dividing by 100 before returning values.
+  price = price * 100;
+  cash = cash * 100;
+  var drawerStart = cid;
+  var change = cash - price;
+
+  function checkDrawerCash(cid) {
+    totalDrawerCash = 0;  //keep this inside the function to make sure you run the function to get a current totalDrawerCash value.
+    for (i=0; i<drawerStart.length; i++){
+      totalDrawerCash += drawerStart[i][1] * 100; 
+      // adds the amount of each demon and adds to totalDrawerCash 
+      // on each iteration through the loop.  * 100 is to add 2 decimal places to each value to use whole numbers
+      // until we are ready to convert them back to dollars and cents.
     }
+    return totalDrawerCash;
   }
-  console.log(totalDrawerCash);
+
+  // Run checkDrawerCash then check to see if we have enough $$ to make change or need to close register.
+  if(totalDrawerCash < change){
+        return("Insufficient Funds");
+      }else if(totalDrawerCash == change){
+        return("Closed");
+      }
+
+  // console.log(totalDrawerCash);
+
+  // Here is your change, ma'am.
   return drawerStart[3];
 }
 
+
+// function makeChange(change){
+//   for (var i = 8; i >= 0; i--) {
+//     if(change > currencyValue[i][1] && drawerStart[i][1]){
+//       var reducedChange = parseFloat(change - currencyValue[i][1].toFixed(2));  
+//       // subtracts the value of the largest denom from change.
+//       // var custChange =  
+//       // will be the array of denoms returned to customer.
+//     }
+//   }
+// }
 
 
 drawer(19.50, 20.00, drawerStart);
@@ -37,38 +55,9 @@ drawer(19.50, 20.00, drawerStart);
 
 
 
-
-
-
-
-
-
-
-
-
-// function drawer(price, cash, cid) {
-//   var change = [];
-//   var drawer = cid;
-//   console.log(drawerStart);
-//   // Here is your change, ma'am.
-//   change = parseFloat(cash - price);
-//   for (i=0; i<drawerStart.length; i++){
-//     totalDrawerCash += parseFloat(drawerStart[i][1].toFixed(2)); 
-//     // adds the amount of each demon and incriments totalDrawerCash 
-//     // on each iteration through the loop.  .toFixed sets the number
-//     // to 2 decimals, but because it turns the number into a string, 
-//     // we run parseFloat() on it to save it as a number so we can do math on it.
-//     if(totalDrawerCash < change){
-//       return("Insufficient Funds");
-//     }else if(totalDrawerCash == change){
-//       return("Closed");
-//     }else{
-//       return change;
-//     }
-//   }
-//   console.log(totalDrawerCash);
-//   return drawerStart[3];
-// }
+//************************************************************************************************************
+//************************************************************************************************************
+//************************************************************************************************************
 
 
 
--- notes.js
+++ notes.js
@@ -1,24 +1,39 @@
+
+
 //Find change amount. change = Cash - price
 // convert change to decimal. parseFloat()
-
-
-
-
-
-
-
-
 //Return the string "Insufficient Funds" if 
 //cash-in-drawer is less than the change due.
 // Sum up cash-in-drawer to compare with changeDue/
-
 //Return the string "Closed" if cash-in-drawer 
 //is equal to the change due.
 
+
+
+
+// parseFloat() and .toFixed are not returning floats to 2 decimal places as expected.
+// Switching to a multiple of 100 to remove the decimals for change.  Divide by 100 before
+// returning the values.
+
+
+
+
+
 //Otherwise, return change in coin and bills, 
 //sorted in highest to lowest order.
+//---------------------------------------------------
+
+//compare change to each index in drawerStart array.
+//When change is < the value of current array index, 
+	//subtract array index value from the index and add 
+	//it to change due.
+	//Push it to a change array to return to the customer.
+	//Repeat until change is 0.
+	//Return the new change array.
 
 
+
+//----------------------------------------------------
 //Compare change to currencyValue to determine highest possible denom to make
 //change.  Subtract that currencyValue amount from change until change is less
 //than currencyValue amount.   Proceed to next lower currencyValue index.
